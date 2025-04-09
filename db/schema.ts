8242da6903cec680422ce087aa7dddae5d713edc
Dicky Darmawan <dicky.darmawan@aksesciptasolusi.com>
2025-04-09 11:18:55 +0700

    remove .env and remove menu

--- db/schema.ts
+++ db/schema.ts
@@ -7,13 +7,6 @@ import {
   timestamp,
 } from "drizzle-orm/pg-core";
 
-export const menu = pgTable("menu", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  parentId: integer("parent_id"),
-  status: boolean("status").default(true),
-});
-
 export const mDirectory = pgTable("m_directory", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
